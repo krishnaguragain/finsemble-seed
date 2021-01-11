0b34bfb4588afc3111b799bf622621044be3dbf5
WatsonCIQ <chris.watson@chartiq.com>
2021-01-11 19:14:56 +0000

    initial commit

---
+++ src/components/jest/jest.ts
@@ -0,0 +1,18 @@
+const FSBLReady = () => {
+	try {
+		// Do things with FSBL in here.
+	} catch (e) {
+		FSBL.Clients.Logger.error(e);
+	}
+};
+
+if (window.FSBL && FSBL.addEventListener) {
+	FSBL.addEventListener("onReady", FSBLReady);
+} else {
+	window.addEventListener("FSBLReady", FSBLReady);
+}
+
+
+export function sum(a: number, b: number): number {
+	return a + b
+}
